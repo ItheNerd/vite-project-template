415deeff43232cd6788a4248a2efef4d928fc346
Jay <99102582+ItheNerd@users.noreply.github.com>
2022-08-16 23:12:40 +0530

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.tailwind.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <h1 className="w-max rounded border-black bg-blue-500 px-[15px] text-3xl font-bold">
+    Hello world
+  </h1>
+);
